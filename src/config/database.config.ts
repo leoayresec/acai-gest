bcb897feb5682f2d95cf32bdd826d7af0215b7d9
leoayresec <leoayresec@gmail.com>
2025-02-13 16:57:48 -0300

    Configurando banco para o Render.

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,14 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import 'dotenv/config';
-import { Empresa } from '../empresa/entities/empresa.entity'
+import { Empresa } from '../empresa/entities/empresa.entity';
 
 export const databaseConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     entities: [Empresa],
-    synchronize: process.env.DB_SYNCHRONIZE === 'true',
-  };
+    synchronize: process.env.DB_SYNCHRONIZE === 'true', 
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+};
